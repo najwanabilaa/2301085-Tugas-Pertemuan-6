840a1a7a401fc5028bf5f98e19d75c8c24fe8d0f
Najwa Nabila H <162705772+najwanabilaa@users.noreply.github.com>
2025-03-16 21:45:23 +0700

    Update main.java

--- main.java
+++ main.java
@@ -2,9 +2,19 @@ package com.oopjava.tugas6;
 
 public class main {
     public static void main(String[] args) {
-       
-        Tugas6 mhs1 = new Tugas6("Najwa", "2301085", "Pendidikan Ilmu Komputer");
-        
-        mhs1.displayInfo();
+        Tugas6 segitiga1 = new Tugas6(1, 1, 1);
+        System.out.println("Segitiga 1:");
+        System.out.println("Luas: " + segitiga1.getLuas());
+        System.out.println("Keliling: " + segitiga1.getKeliling());
+
+        Tugas6 segitiga2 = new Tugas6(30, 10, 30);
+        System.out.println("\nSegitiga 2:");
+        System.out.println("Luas: " + segitiga2.getLuas());
+        System.out.println("Keliling: " + segitiga2.getKeliling());
+
+        Tugas6 segitiga3 = new Tugas6(20, 10, 60);
+        System.out.println("\nSegitiga 3:");
+        System.out.println("Luas: " + segitiga3.getLuas());
+        System.out.println("Keliling: " + segitiga3.getKeliling());
     }
 }
