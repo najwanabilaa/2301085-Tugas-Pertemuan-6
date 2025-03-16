739b47f8f04df41a4889a0a94504b2bb523d2c7d
Najwa Nabila H <162705772+najwanabilaa@users.noreply.github.com>
2025-03-16 21:22:36 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+package com.oopjava.tugas6;
+
+public class main {
+    public static void main(String[] args) {
+       
+        Tugas6 mhs1 = new Tugas6("Najwa", "2301085", "Pendidikan Ilmu Komputer");
+        
+        mhs1.displayInfo();
+    }
+}
